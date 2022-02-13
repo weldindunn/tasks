216fc5038758fd4e0e88b0478849c0e1a0197a31
Weldin Dunn <weldindunn20@gmail.com>
2022-02-12 22:58:14 -0500

    Completed Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,40 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                <h2>UD CISC275 with React Hooks and TypeScript</h2>
             </header>
             <p>Well, this is neat. -Weldin</p>
-            <Button onClick={() => console.log("Logged as a lumberjack.")}>
-                Click me!
+            <p>Also, Hello World</p>
+            <p>Guess I&apos;ll rank the Indiana Jones movies</p>
+            <ol>
+                <li>Last Crusade</li>
+                <li>Raiders of the Lost Ark</li>
+                <li>Temple of Doom</li>
+                <li>Kingdom of the Crystal Skull</li>
+            </ol>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
             </Button>
             <Container>
                 <Row>
-                    <Col>What&apos;s this, some sort of column?</Col>
                     <Col>
-                        Second column! Here&apos;s a meme I found:
-                        <img
-                            src="../assets/images/meme.jpg"
-                            alt="It's a history meme."
+                        <div
+                            style={{
+                                width: "600px",
+                                height: "150px",
+                                backgroundColor: "#C83030",
+                                margin: "auto"
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "600px",
+                                height: "150px",
+                                backgroundColor: "#C83030",
+                                margin: "auto"
+                            }}
                         />
                     </Col>
                 </Row>
