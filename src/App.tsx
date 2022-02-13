e7196fb7d0d2a605e1b031d4e61ec8527bd8209b
Weldin Dunn <weldindunn20@gmail.com>
2022-02-12 22:19:59 -0500

    Starting task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -7,11 +8,22 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Oh and Weldin was here.
-            </p>
-            <p>Hello World</p>
+            <p>Well, this is neat. -Weldin</p>
+            <Button onClick={() => console.log("Logged as a lumberjack.")}>
+                Click me!
+            </Button>
+            <Container>
+                <Row>
+                    <Col>What&apos;s this, some sort of column?</Col>
+                    <Col>
+                        Second column! Here&apos;s a meme I found:
+                        <img
+                            src="../assets/images/meme.jpg"
+                            alt="It's a history meme."
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
