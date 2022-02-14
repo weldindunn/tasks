0b8d8bb39c4cc19bb7b23900932507cb461e05d2
Weldin Dunn <weldindunn20@gmail.com>
2022-02-14 10:12:48 -0500

    Revert "Solved html css"

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ function App(): JSX.Element {
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
             </Button>
-            <img src="../assets/images/meme.jpg" alt="A history meme" />
             <Container>
                 <Row>
                     <Col>
