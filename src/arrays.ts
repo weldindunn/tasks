a3270f0feca0c496aff943bf39c6e0925b02cb3e
Weldin Dunn <weldindunn20@gmail.com>
2022-02-19 00:39:00 -0500

    Just working on a few bugs that don't matter but make it so I can't merge

--- src/arrays.ts
+++ src/arrays.ts
@@ -1,3 +1,5 @@
+import { constants } from "buffer";
+
 /**
  * Consume an array of numbers, and return a new array containing
  * JUST the first and last number. If there are no elements, return
@@ -33,7 +35,7 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    let nums = numbers.map((num: string): number => ~~num);
+    const nums = numbers.map((num: string): number => ~~num);
     return nums;
 }
 
@@ -45,10 +47,10 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    let vals = amounts.map((val: string): string =>
+    const vals = amounts.map((val: string): string =>
         val.substring(0, 1) === "$" ? val.substring(1) : val
     );
-    let nums = vals.map((num: string): number => ~~num);
+    const nums = vals.map((num: string): number => ~~num);
     return nums;
 };
 
@@ -70,8 +72,8 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    let wrds = words.filter((word: string): boolean => word.length < 4);
-    let sum = wrds.reduce((total: number, word: string) => total + 1, 0);
+    const wrds = words.filter((word: string): boolean => word.length < 4);
+    const sum = wrds.reduce((total: number, word: string) => total + 1, 0);
     return sum;
 }
 
@@ -81,7 +83,7 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    let cols = colors.every(
+    const cols = colors.every(
         (col: string): boolean =>
             col === "blue" || col === "red" || col === "green"
     );
@@ -99,7 +101,7 @@ export function makeMath(addends: number[]): string {
     if (addends.length < 1) {
         return "0=0";
     } else {
-        let sum = addends.reduce(
+        const sum = addends.reduce(
             (total: number, num: number) => total + num,
             0
         );
@@ -117,20 +119,23 @@ export function makeMath(addends: number[]): string {
  * And the array [1, 9, 7] would become [1, 9, 7, 17]
  */
 export function injectPositive(values: number[]): number[] {
-    let vals = [...values];
+    const vals = [...values];
     if (!values.find((value: number): boolean => value < 0)) {
-        let sum = values.reduce((total: number, val: number) => total + val, 0);
+        const sum = values.reduce(
+            (total: number, val: number) => total + val,
+            0
+        );
         vals.push(sum);
     } else {
-        let index = vals.findIndex((value: number): boolean => value < 0); //Finds the negative number
+        const index = vals.findIndex((value: number): boolean => value < 0); //Finds the negative number
 
-        let temp = vals.filter(
+        const temp = vals.filter(
             (val: number): boolean =>
                 vals.findIndex((value: number): boolean => value === val) <
                 index
         ); //Gets rid of numbers after it
 
-        let sum = temp.reduce((total: number, val: number) => total + val, 0);
+        const sum = temp.reduce((total: number, val: number) => total + val, 0);
         vals.splice(index + 1, 0, sum);
     }
 
