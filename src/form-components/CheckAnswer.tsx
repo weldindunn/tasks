1b3a80392ee9341c2ff3a038be88ce1919c38801
Weldin Dunn <weldindunn20@gmail.com>
2022-03-09 18:57:57 -0500

    Completed CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,35 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: ChangeEvent) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="answerChecker">
+                <Form.Label></Form.Label>
+                <Form.Control
+                    value={answer}
+                    onChange={updateAnswer}
+                    placeholder="Enter Answer"
+                />
+            </Form.Group>
+            <div>
+                {answer === expectedAnswer ? <span>✔️</span> : <span>❌</span>}
+            </div>
         </div>
     );
 }
