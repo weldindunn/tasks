845030c709996b9c8b123db98f45505fd6a72ca6
Weldin Dunn <weldindunn20@gmail.com>
2022-03-18 18:06:53 -0400

    Fixed data importing issues, added sketch

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { Question } from "../quizzer-interfaces/question";
+import { Question, QuestionType } from "../quizzer-interfaces/question";
 import { Quiz } from "../quizzer-interfaces/quiz";
 import { QuizList } from "../quizzer-components/QuizList";
-import quizSamples from "../data/quizzes.json";
+import quizzes from "./quizzes.json";
+import questions from "./questions.json";
+import sketch from "./sketch.jpg";
 
-//const QUIZZES = quizSamples.map((quiz: Quiz): Quiz => ({ ...quiz }));
+const QUIZZES = quizzes.map((quiz): Quiz => ({ ...quiz }));
+const QUESTIONS = questions.map(
+    (question): Question => ({ ...question, options: [] })
+);
 
 export function Quizzer(): JSX.Element {
-    const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+    const [quizzes, setQuizzes] = useState<Quiz[]>(QUIZZES);
 
     return (
         <div>
             <h3>Quizzer</h3>
             <QuizList quizzes={quizzes}></QuizList>
             <Button>Add Quiz</Button>
+
+            <div>
+                <img src={sketch} width="600" height="auto" />
+            </div>
         </div>
     );
 }
