7eb25322aef7e06a7d7afdcfa31596b806aec4cf
Weldin Dunn <weldindunn20@gmail.com>
2022-03-18 22:58:23 -0400

    Revert "Created basic layout for Quizzer"

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ import { GiveAttempts } from "./form-components/GiveAttempts";
 import { EditMode } from "./form-components/EditMode";
 import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 import { ChangeColor } from "./form-components/ChangeColor";
-import { Quizzer } from "./quizzer/Quizzer";
 
 function App(): JSX.Element {
     return (
@@ -23,7 +22,6 @@ function App(): JSX.Element {
             <header className="App-header">
                 <h2>UD CISC275 with React Hooks and TypeScript</h2>
             </header>
-            <Quizzer></Quizzer>
             <hr></hr>
             <CheckAnswer expectedAnswer="42"></CheckAnswer>
             <hr></hr>
